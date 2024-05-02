170e26fc755291c9fdb3618176fca498d9e5845e
elena-6over6 <elena@6over6.com>
2024-05-02 17:04:56 +0300

    [release-5.48.1] updated version and checksum

--- Package.swift
+++ Package.swift
@@ -39,8 +39,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GlassesOnSDK",
-            url: "https://glasseson.jfrog.io/artifactory/luna-glasseson-sdk-ios-local/5.46.0/GlassesOnSDK.xcframework.zip",
-            checksum: "c707291454ca7f09ca02475ffa966c1471cfebc7e1db19accfd300ba64af3af1"
+            url: "https://glasseson.jfrog.io/artifactory/luna-glasseson-sdk-ios-local/5.48.1-4670/GlassesOnSDK.xcframework.zip",
+            checksum: "c10034c1bcd942eb78f6d8cc3752deb3482e50adbbbcec20976eb619b4d877ac"
         )
     ]
 )
