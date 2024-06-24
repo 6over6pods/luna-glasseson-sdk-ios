6572db4cf6dfc054aac6287ad388a59662ec8fb0
deployer-6over6 <deployer-6over6@6over6.com>
2024-06-24 11:06:20 +0000

    deployer-6over6: opening branch with changes in Package.swift

--- Package.swift
+++ Package.swift
@@ -39,8 +39,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GlassesOnSDK",
-            url: "https://glasseson.jfrog.io/artifactory/luna-glasseson-sdk-ios-local/5.46.0/GlassesOnSDK.xcframework.zip",
-            checksum: "c707291454ca7f09ca02475ffa966c1471cfebc7e1db19accfd300ba64af3af1"
+            url: "https://glasseson.jfrog.io/artifactory/luna-glasseson-sdk-ios-local/5.52.0-4673/GlassesOnSDK.xcframework.zip",
+            checksum: "afbcbe8bff265a978a1496a6677ec7dbee69646052d04b434976acb8fd079be5"
         )
     ]
 )
